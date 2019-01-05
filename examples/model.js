e179b5b2de84b706238631833c7baaf4161eb48a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2019-01-05 18:26:52 +0000

    handling filters and data extractions

--- examples/model.js
+++ examples/model.js
@@ -2,15 +2,22 @@ const TOKEN = '';
 
 const db = require('../src/db').db(TOKEN);
 
-db.post('users', 3, {
-    ciao: 3
-}).then(({data}) => {
-    console.log('POST', data);
-    db.getOne('users', 1).then(({data}) => {
+db.post('users', 19, {ciao: 19}).then(() => {
+    console.log('POST', 'ok');
+    db.getOne('users', 1).then((data) => {
         console.log('GETONE', data);
     });
+    /*
 
-    db.get('users', 1).then(({data}) => {
+    db.order('users', 'ciao').then(({data}) => {
+        console.log('ORDER', data);
+    });
+    
+    db.filter('users', 'ciao', 3).then(({data}) => {
+        console.log('FILTER', data);
+    });
+    */
+    db.get('users').then((data) => {
         console.log('GET', data);
     });
-});
+}).catch(error => console.log(error));
--- src/db.js
+++ src/db.js
@@ -11,18 +11,37 @@ const db = (token) => {
     });
 
     return {
-        get(modelName, filter = {}, orderBy = {}) {
-            return instance.get(`${modelName}`);
+        order(modelName, orderKey) {
+            return instance.get(`${modelName}?orderKey=${orderKey}`)
+            .then(this.handleResult);
+        },
+        filter(modelName, name, value) {
+            return instance.get(`${modelName}?orderKey=${name}&filterValue=${value}&valueType=number`)
+            .then(this.handleResult);
+        },
+        get(modelName) {
+            return instance.get(`${modelName}`)
+            .then(this.handleResult);
         },
         getOne(modelName, id) {
-            return instance.get(`${modelName}/${id}`);
+            return instance.get(`${modelName}/${id}`)
+            .then(this.handleResult);
         },
         post(modelName, id, body) {
-            return instance.post(`${modelName}/${id}`, body);
+            return instance.post(`${modelName}/${id}`, body)
+            .then(this.handleResult);
         },
         delete(modelName, id) {
-            return instance.delete(`${modelName}/${id}`);
+            return instance.delete(`${modelName}/${id}`)
+            .then(this.handleResult);
         },
+        handleResult({status, data}) {
+            return new Promise((resolve,reject)=> {
+                if(status > 299) reject(status);
+                if(('ok' in data) && data.ok === true) return resolve(data.result || true);
+                reject(data.error || 'Unexpected Error');
+            })
+        }
     };
 };
 
